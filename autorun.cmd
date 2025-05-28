73a5807d22b86c2d69cb9c2a5e8aa06e0f635a53
kblee0 <kblee0@gmail.com>
2025-05-29 01:44:31 +0900

    update

--- autorun.cmd
+++ autorun.cmd
@@ -2,8 +2,12 @@
 doskey ls=dir $*
 doskey cat=type $*
 doskey cd=cd /d $*
-doskey fcp=C:\usr\local\FastCopy\fcp.exe $*
 doskey notepad="C:\home\local\npp\notepad++.exe" $*
+doskey rcopy=robocopy $1 $2 /E /SJ /SL /MT /COPY:DAT /DCOPY:DAT /R:1 /W:1 /NFL $3 $4 $5 $6 $7 $8 $9
+doskey rsync=robocopy $1 $2 /E /SJ /SL /MIR /MT /COPY:DAT /DCOPY:DAT /R:1 /W:1 /NFL $3 $4 $5 $6 $7 $8 $9
+doskey history=doskey /history
+
 IF NOT EXIST c:\windows\system32\sudo.exe doskey sudo=c:\usr\bin\gsudo.exe $*
 IF EXIST C:\home\android\platform-tools\adb.exe doskey adb=C:\home\android\platform-tools\adb.exe $*
-@echo on 
+
+SET PATH=%PATH%;C:\home\bin;C:\home\local\bin;C:\home\proj\win_batch
