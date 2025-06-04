583d05271a1e0f3d7e5b50e0e706fa2999534390
kblee0 <kblee0@gmail.com>
2025-06-04 22:45:52 +0900

    update

--- devenv.bat
+++ devenv.bat
@@ -1,79 +1,79 @@
 @echo off
 
-rem %SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;%SYSTEMROOT%\System32\OpenSSH\
+:: %SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;%SYSTEMROOT%\System32\OpenSSH\
 
 for /f "skip=2 tokens=2,*" %%A in ('reg query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path 2^>nul') do (
   SET SYS_PATH=%%B
 )
 
-rem -------------------------------------------
-rem Jetbrain
-rem -------------------------------------------
+:: -------------------------------------------
+:: Jetbrain
+:: -------------------------------------------
 
 setx /M IDEA_PROPERTIES     c:\home\dev\.config\IntelliJ.properties
 setx /M PYCHARM_PROPERTIES  c:\home\dev\.config\PyCharm.properties
 setx /M DATAGRIP_PROPERTIES c:\home\dev\.config\DataGrip.properties
 setx /M WEBIDE_PROPERTIES   c:\home\dev\.config\WebStorm.properties
 
-rem -------------------------------------------
-rem JAVA
-rem -------------------------------------------
+:: -------------------------------------------
+:: JAVA
+:: -------------------------------------------
 
 setx /M JAVA_HOME C:\home\dev\Java\jdk-21.0.4+7
 set SYS_PATH=%SYS_PATH%;%%JAVA_HOME%%\bin
 
-rem -------------------------------------------
-rem Maven
-rem -------------------------------------------
+:: -------------------------------------------
+:: Maven
+:: -------------------------------------------
 
 setx /M M2_HOME C:\home\dev\apache-maven-3.9.9
 
 set SYS_PATH=%SYS_PATH%;%%M2_HOME%%\bin
 
 
-rem -------------------------------------------
-rem Gradle
-rem -------------------------------------------
+:: -------------------------------------------
+:: Gradle
+:: -------------------------------------------
 
 setx /M GRADLE_HOME C:\home\dev\gradle-8.13
 setx /M GRADLE_USER_HOME C:\home\dev\.data\gradle\repo
 
 set SYS_PATH=%SYS_PATH%;%%GRADLE_HOME%%\bin
 
-rem -------------------------------------------
-rem Node
-rem -------------------------------------------
+:: -------------------------------------------
+:: Node
+:: -------------------------------------------
 
 setx /M NVM_HOME c:\home\dev\nvm
 set SYS_PATH=%SYS_PATH%;%%NVM_HOME%%;%%NVM_HOME%%\nodejs\.npm\global;%%NVM_HOME%%\nodejs
 
-rem -------------------------------------------
-rem Python
-rem -------------------------------------------
+:: -------------------------------------------
+:: Python
+:: -------------------------------------------
 
 setx /M PYTHON_HOME C:\home\dev\Python
 setx /M VIRTUAL_ENV C:\home\dev\.data\.venv
 set SYS_PATH=%SYS_PATH%;%%PYTHON_HOME%%\Scripts;%%PYTHON_HOME%%;%%VIRTUAL_ENV%%\Scripts
 
-rem -------------------------------------------
-rem Git
-rem -------------------------------------------
+:: -------------------------------------------
+:: Git
+:: -------------------------------------------
 setx /M GITHUB_TOKEN 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
 set SYS_PATH=%SYS_PATH%;C:\home\dev\git\bin
 
-rem -------------------------------------------
-rem SVN
-rem -------------------------------------------
+:: -------------------------------------------
+:: SVN
+:: -------------------------------------------
 set SYS_PATH=%SYS_PATH%;C:\home\dev\svn\bin
 
 
-rem --------- Global Path --------
+:: --------- Global Path --------
 setx /M PATH "%SYS_PATH%"
 
 
-rem -------------------------------------------
-rem User setting
-rem -------------------------------------------
+:: -------------------------------------------
+:: User setting
+:: -------------------------------------------
 echo -------------------------------------------
 echo type below command unber user command env
 echo -------------------------------------------
