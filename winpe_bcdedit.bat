c0ced0a0eb5827595d104bc3426a9a91c4d81fd9
kblee0 <kblee0@gmail.com>
2025-06-05 08:53:10 +0900

    update

---
+++ winpe_bcdedit.bat
@@ -0,0 +1,22 @@
+@echo off
+:: 참고: https://cappleblog.tistory.com/103
+:: {ramdiskoptions} 항목을 생성합니다. 이 때 /d 의 내용은 없앨 것이기에 대충 설정하셔도 됩니다.
+:: bcdedit /enum {ramdiskoptions} 명령으로 설정 확인
+bcdedit /create {ramdiskoptions} /d "Ramdisk Options"
+:: 준비된 boot.sdi 파일의 전체 경로에 맞춰 전체 장치 경로를 설정합니다.
+bcdedit /set {ramdiskoptions} ramdisksdidevice partition=D:
+bcdedit /set {ramdiskoptions} ramdisksdipath \img\boot.sdi
+bcdedit /deletevalue {ramdiskoptions} description
+
+for /f %%i in ('bcdedit /create /d "Windows PE" /application osloader') DO set GUID=%%i
+bcdedit /create /d "Windows PE" /application osloader
+bcdedit /set %GUID% device ramdisk=[D:]\img\boot.wim,{ramdiskoptions}
+bcdedit /set %GUID% path \windows\system32\winload.efi
+bcdedit /set %GUID% description "Windows PE"
+bcdedit /set %GUID% locale "ko-kr"
+bcdedit /set %GUID% osdevice ramdisk=[D:]\img\boot.wim,{ramdiskoptions}
+bcdedit /set %GUID% systemroot \windows
+bcdedit /set %GUID% winpe yes
+bcdedit /set %GUID% nx OptIn
+bcdedit /set %GUID% detecthal yes
+bcdedit /displayorder %GUID% /addlast
